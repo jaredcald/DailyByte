510bc52e907f0e29497e18aef26ea22677419374
Jared_Caldwell <jared.c.caldwell@gmail.com>
2020-09-28 19:56:23 -0400

    Added TwoSum solution and unit tests

---
+++ HashMaps/src/test/com/technicalquestions/TwoSumTest.java
@@ -0,0 +1,32 @@
+package com.technicalquestions;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TwoSumTest {
+
+    TwoSum twoSum = new TwoSum();
+    int[][] testIntArray = new int[][]{
+            new int[]{1, 3, 8, 2},
+            new int[]{3, 9, 13, 7},
+            new int[]{4, 2, 6, 5, 2}};
+    int[] testTargetSums = new int[]{10, 8, 4};
+
+    @Test
+    void TwoSumTest1(){
+        assertTrue(twoSum.twoSum(testIntArray[0],testTargetSums[0]));
+    }
+
+    @Test
+    void TwoSumTest2(){
+        assertFalse(twoSum.twoSum(testIntArray[1],testTargetSums[1]));
+    }
+
+    @Test
+    void TwoSumTest3(){
+        assertTrue(twoSum.twoSum(testIntArray[2],testTargetSums[2]));
+    }
+}
